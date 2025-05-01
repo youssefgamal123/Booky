ce13c0d4334df567218ddb3cd777cdfafe9448bd
Youssef Gamal-AbdelNasser <66358331+youssefgamal123@users.noreply.github.com>
2025-05-01 16:19:06 +0300

    Implemented book services API

---
+++ src/main/java/com/Application/Booky/service/BookMapper.java
@@ -0,0 +1,60 @@
+package com.Application.Booky.service;
+
+
+import com.Application.Booky.entity.Book;
+import com.Application.Booky.entity.BookTransactionHistory;
+import com.Application.Booky.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookMapper {
+
+
+    public Book toBook(BookRequest request , User user) {
+
+
+        return new Book(
+                request.title(),
+                request.authorName(),
+                request.Shareable(),
+                request.isbn(),
+                request.synopsis(),
+                request.bookCover(),
+                request.archived(),
+                user
+
+        ) ;
+
+
+
+
+    }
+
+    public BookResponse toBookResponse(Book book) {
+        return BookResponse.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .authorName(book.getAuthorName())
+                .isbn(book.getIsbn())
+                .synopsis(book.getSynopsis())
+                .archived(book.isArchived())
+                .shareable(book.isShareable())
+                .owner(book.getOwner().getFirstname())
+                .build();
+    }
+
+
+    public BorrowedBookResponse toBorrowedBookResponse(BookTransactionHistory history) {
+        return BorrowedBookResponse.builder()
+                .id(history.getBook().getId())
+                .title(history.getBook().getTitle())
+                .authorName(history.getBook().getAuthorName())
+                .isbn(history.getBook().getIsbn())
+                .returned(history.isReturned())
+                .returnApproved(history.isReturnApproved())
+                .build();
+    }
+
+
+
+}
---
+++ src/main/java/com/Application/Booky/service/BookRequest.java
@@ -0,0 +1,35 @@
+package com.Application.Booky.service;
+
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record BookRequest(
+
+
+
+
+        @NotNull(message = "100")
+        @NotEmpty(message = "100")
+        String title,
+
+        @NotNull(message = "101")
+        @NotEmpty(message = "101")
+        String authorName,
+
+
+        boolean Shareable,
+
+        String isbn,
+        String synopsis,
+        String bookCover,
+        boolean archived
+
+
+
+
+        )
+
+
+{
+}
