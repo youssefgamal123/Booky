9f76bbd2ed31097763ff6ba6db2f2947f955d3b6
Youssef Gamal-AbdelNasser <66358331+youssefgamal123@users.noreply.github.com>
2025-05-01 15:56:13 +0300

    Implemented JWT-based security configuration

---
+++ src/main/java/com/Application/Booky/Security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.Application.Booky.Security;
+
+
+import com.Application.Booky.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    private final UserRepository repository;
+
+    public UserDetailsServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByemail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+
+
+
+}
